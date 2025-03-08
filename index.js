f5657debe99f1f33b9e03bf1a00cf27b67473b6a
venkat ravilla <venkatravilla333@gmail.com>
2025-03-08 13:28:13 +0530

    core modules

--- index.js
+++ index.js
@@ -28,3 +28,113 @@
 
 // console.log('hello')
 
+
+
+//Custom module
+
+// const os = require('os')
+
+// console.log(os.totalmem())
+// console.log(os.freemem())
+// console.log(os.arch())
+// console.log(os.platform())
+// console.log(os.cpus())
+// console.log(os.release())
+// console.log(os.uptime())
+// console.log(os.networkInterfaces())
+
+//fs module
+
+const fs = require('fs')
+
+
+// fs.readFile('./index.js', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(data)
+//   }
+// })
+
+// console.log('hello sachin')
+
+// try {
+//   fs.readFileSync('./index.js')
+// } catch (error) {
+//   console.log(error)
+// }
+
+// fs.writeFile('./index.js', 'hello kohli', (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('writabale successfully')
+//   }
+// })
+
+
+// try {
+//   fs.writeFileSync('./index.js', 'sachin')
+// } catch (error) {
+//   console.log(error)
+// }
+
+
+// fs.appendFile('./index.js', 'sachin', (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('new data added')
+//   }
+// })
+
+// fs.unlink('./index.js', (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('file deleted')
+//   }
+// })
+
+// fs.mkdir('/new', (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('folder created')
+//   }
+// fs.rmdir('./hello1', (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('folder removed')
+//   }
+// })
+
+// const path = require('path')
+
+
+// console.log(path.extname('./myfolder/index.html'))
+// console.log(path.basename('./myfolder/index.html'))
+// console.log(path.dirname('./myfolder/index.html'))
+
+// const http = require('http')
+
+// let server = http.createServer((req, res) => {
+//   if (req.url === '/home') {
+//     res.write('response from server from home')
+//     res.end()
+//   }
+
+//   if (req.url === '/products') {
+//     res.write(JSON.stringify([{ pname: 'apple', price: 2000 }, { pname: 'banana', price: 1000 }]))
+//     res.end()
+//   }
+
+// })
+
+// server.listen(5500, () => {
+//   console.log('server started port 5500')
+// })
+
+
+
